ec083ec9ba09ab9d982d326cc304b56f2527c971
Darrel Pol <darrel.gaming.hk@gmail.com>
2019-01-23 20:21:45 -0600

    Working with Data and Angular Material

--- fitness-tracker/src/app/navigation/sidenav-list/sidenav-list.component.ts
+++ fitness-tracker/src/app/navigation/sidenav-list/sidenav-list.component.ts
@@ -1,20 +1,37 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, OnDestroy } from '@angular/core';
+import { AuthService } from 'src/app/auth/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-sidenav-list',
   templateUrl: './sidenav-list.component.html',
   styleUrls: ['./sidenav-list.component.css']
 })
-export class SidenavListComponent implements OnInit {
-  @Output() closeSideNav = new EventEmitter<void>();
 
-  constructor() { }
+export class SidenavListComponent implements OnInit, OnDestroy {
+  @Output() closeSideNav = new EventEmitter<void>();
+  isAuth: boolean = false;
+  authSubscription: Subscription;
+
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+    this.authSubscription = this.authService.authChange.subscribe(authStatus => {
+      this.isAuth = authStatus;
+    });
+  }
+
+  ngOnDestroy() {
+    this.authSubscription.unsubscribe();
   }
 
   onCloseSideNav() {
     this.closeSideNav.emit();
+  }
+
+  onLogout(){
+    this.onCloseSideNav();
+    this.authService.logout();
   }
 
 }
